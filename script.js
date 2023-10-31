abb2082809730ad8dfe67e696a01cd7873bd0a95
Amritanshu21 <120729168+Amritanshu21@users.noreply.github.com>
2023-10-31 15:34:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+gsap.from("#nav",{
+    scale:2,duration:1.2
+}) 
+
+gsap.from("#centre ",{
+    y:-1200,
+    duration:1.8,
+delay:2
+})
+
+gsap.from("#centre h1",{
+    x:-1200,
+    duration:1.8,
+delay:2.5
+})
